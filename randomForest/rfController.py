2ab924fdc39691d13dfb01bcc526db302732b747
ClimbsRocks <climbsbytes@gmail.com>
2015-09-29 23:27:23 -0700

    reads in data and prepares it for a random forest classifier

--- randomForest/rfController.py
+++ randomForest/rfController.py
@@ -6,3 +6,38 @@
     # figure out if the data is categorical or numerical
     
 from sys import argv
+from sklearn.ensemble import RandomForestClassifier
+import csv
+
+trainingData = []
+targetData = []
+
+targetDataFileName='/Users/preston/ghLocal/machineLearningWork/ppComplete/randomForest/pythonOutputVectkaggleGiveCredit.csv'
+inputDataFileName='/Users/preston/ghLocal/machineLearningWork/ppComplete/randomForest/pythonInputVect2kaggleGiveCredit.csv'
+
+with open(inputDataFileName, 'rU') as openInputFile:
+    inputRows = csv.reader(openInputFile)
+    for row in inputRows:
+        for value in row:
+            if value == 'nan'
+
+        trainingData.append(row)
+
+
+with open(targetDataFileName, 'rU') as openOutputFile:
+    outputRows = csv.reader(openOutputFile)
+    for row in outputRows:
+        try:
+            row[0] = float(row[0])
+        except:
+            row[0] = row[0]
+        targetData.append(row[0])
+print trainingData
+
+
+rf = RandomForestClassifier(n_estimators=150)
+# TODO:
+  # load in the two datasets
+
+rf.fit(trainingData, targetData)
+
