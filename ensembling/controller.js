a10e4ed0b0254328a4a2053bdf78eebb80d860e8
ClimbsRocks <climbsbytes@gmail.com>
2015-10-04 15:05:21 -0700

    calculates predictions in flexible way

--- ensembling/controller.js
+++ ensembling/controller.js
@@ -7,7 +7,13 @@ var argv;
 
 module.exports = {
   createEnsemble: function(globalArgs) {
-    utils.consolidateFiles(globalArgs);
+    utils.consolidateFiles(globalArgs, function() {
+      // generate the set that is the combination of all the algos we've trained so far
+      // iterate through each row
+        // for each item in that combination set, run through our whole ensembling logic, calculating the error rate for that row for that particular ensembling method
+      // use the best set and ensembling method to calculate scores for each item
+      // write to a file. 
+    });
   }
 
 
--- ensembling/utils.js
+++ ensembling/utils.js
@@ -1,32 +1,64 @@
 var csv = require('csv');
 var fs = require('fs');
 var path = require('path');
-var predictions = {};
+var summary = {};
+var ensembleMethods = require('./ensembleMethods.js');
 
 module.exports = {
 
-  consolidateFiles: function(globalArgs) {
-    // eventually we're just going to automatically gather all of the .csv files in this folder. hardcoding for now. 
-    var fileName = 'neuralNetwork.csv';
+  consolidateFiles: function(globalArgs, callback) {
 
-    var filePath = path.join(globalArgs.ppCompleteLocation,'predictions',fileName);
-    var firstRow = true;
-    var parser = csv.parse({}, function(err, row) {
-      if (firstRow) {
-        firstRow = false;
-        // skip it! 
+    fs.readdir(path.join(globalArgs.ppCompleteLocation,'predictions'), function(err,files) {
+      if (err) {
+        console.error('there are no files in the "Predictions" folder. We need the predicted output from the classifiers in that folder in order to ensemble the results together. please run this library again, or copy/paste the results into the Predictions folder, to create an ensemble.');
       } else {
-        console.log('row:',row);
-        var id = row[0];
-        if(predictions[id] === undefined) {
-          predictions[id] = {};
-        }
-        predictions[id].fileName = row[1];
-        
+        console.log('files from Prections:',files);
+        files.forEach(function(fileName) {
+
+          if (fileName.slice(-4).toLowerCase() === 'csv') {
+            var filePath = path.join(globalArgs.ppCompleteLocation,'predictions',fileName);
+            var firstRow = true;
+            var parser = csv.parse({}, function(err, row) {
+              if (firstRow) {
+                firstRow = false;
+                // skip it! 
+              } else {
+                console.log('row:',row);
+                var id = row[0];
+                if(summary[id] === undefined) {
+                  summary[id] = {};
+                }
+                summary[id].fileName = row[1];
+                
+              }
+            });
+            
+            fs.createReadStream(filePath).pipe(parser);
+          }
+
+        });
       }
     });
-
-    fs.createReadStream(filePath).pipe(parser);
     
+  },
+
+  calculateAggregatedPredictions: function(algoNames, bestMethod) {
+    var results = [];
+    for (var row in summary) {
+      // pick out only the predictions from the algos that were selected by createEnsemble:
+      var eligiblePredictions = [];
+      algoNames.forEach(function(name) {
+        eligiblePredictions.push(row[name]);
+      });
+      // ensemble methods holds all the ways we have of ensembling together the results from different predictions. 
+      // each method takes in an array, and returns a single number
+      var output = ensembleMethods[bestMethod](eligiblePredictions);
+      results.push([row.ID, output]);
+    }
+    return results;
+  },
+
+  writeToFile: function() {
+
   }
 };
