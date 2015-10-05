55d33d4689410281f49a2d99fc00dd5512b86b13
ClimbsRocks <climbsbytes@gmail.com>
2015-10-04 22:06:50 -0700

    modularizes makeTrainingObj

--- neuralNet/trainingUtils.js
+++ neuralNet/trainingUtils.js
@@ -41,39 +41,7 @@ module.exports = {
     return allParamsToTest;
   },
 
-  makeTrainingObj: function ( hlArray) {
-    var trainingObj = {
-      errorThresh: 0.05,  // error threshold to reach
-      iterations: 1000,   // maximum training iterations
-      // log: true,           // console.log() progress periodically
-      // logPeriod: 1,       // number of iterations between logging
-      learningRate: 0.6    // learning rate
-    };
-
-    var brainID = ++nn.totalRunningNets;
-
-    // TODO TODO: Finish making argv.copyData functional
-    if(argv.copyData) {
-      var fileName = '/formattedData' + (brainID % numCPUs) + '.txt';
-    } else {
-      // TODO: rename this file to make it more user-friendly
-      var fileName = 'formattingData3.txt';
-    }
-    var pathToChildData = path.join(nnLocation,fileName);
-
-    var totalMessageObj = {
-      type: 'startBrain',
-      brainID: brainID,
-      hiddenLayers: hlArray, 
-      trainingObj: trainingObj, 
-      pathToData: pathToChildData, 
-      totalRows: nn.dataSummary.totalRows,
-      maxTrainingTime: nn.maxChildTrainingTime,
-      maxTrainingIterations: nn.maxChildTrainingIterations,
-    };
-
-    return totalMessageObj;
-  },
+  makeTrainingObj: require('./utils/makeTrainingObj.js'),
 
   setGlobalVars: require('./utils/setGlobalVars.js'),
 
