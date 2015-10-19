5c0ad6e4f7edf214716e7e04e433fbc885394095
ClimbsRocks <climbsbytes@gmail.com>
2015-10-18 17:11:54 -0700

    mocks out the results from data-formatter to allow for more rapid testing

---
+++ pySetup/testingFileNames.js
@@ -0,0 +1,7 @@
+module.exports = { 
+  id_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_train_kaggleGiveCredit.csv',
+  y_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/y_train_kaggleGiveCredit.csv',
+  id_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_test_kaggleGiveCreditTest.csv',
+  X_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_testkaggleGiveCreditTest.csv',
+  X_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_trainkaggleGiveCredit.csv' 
+};
