17aef383d8db62eebabc7cb7d1e348f660bf25cb
ClimbsRocks <climbsbytes@gmail.com>
2015-10-28 22:01:13 -0700

    delete parameters that have been renamed in XGBoosts's api

--- pySetup/parameterMakers/clXGBoost.py
+++ pySetup/parameterMakers/clXGBoost.py
@@ -7,15 +7,15 @@ def makeParams(X, y, globalArgs, dev):
         # max_features
 
     parameters_to_try = {
-        'num_round': [100, 250, 500],
-        'eta': [0.05, 0.1, 0.3],
+        # 'num_round': [100, 250, 500],
+        # 'eta': [0.05, 0.1, 0.3],
         'max_depth': [6, 9, 12],
         'subsample': [0.9, 1.0],
         'colsample_bytree': [0.9, 1.0]
     }
 
-    if dev:
-        parameters_to_try.pop('num_round', None)
-        parameters_to_try.pop('eta', None)
+    # if dev:
+        # parameters_to_try.pop('num_round', None)
+        # parameters_to_try.pop('eta', None)
         
     return parameters_to_try
