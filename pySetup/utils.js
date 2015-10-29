24634a666aedb32eb6d057ddd9bc3c5f4d864196
ClimbsRocks <climbsbytes@gmail.com>
2015-10-28 21:59:13 -0700

    renames processes.js to utils.js and handles event emitting differently

--- pySetup/utils.js
+++ pySetup/utils.js
@@ -1,53 +1,81 @@
 var py = global.pythonNamespace;
-
+var argv = global.argv;
 var path = require('path');
-var PythonShell = require('ppc-python-shell');
-
-var rfLocation = py.rfLocation;
+var utilsPyShell = require('./utilsPyShell.js');
+var df = require('data-formatter');
 
 module.exports = {
-  expectedMessages: {
-    dictVectMapping: true,
-    fileNames: true
-  },
-  attachLogListener: function(referenceToShell) {
-    referenceToShell.on('message', function(message) {
-      if(message.type === 'console.log') {
-        console.log('snake says:',message.text);
-      }
-      else if ( !module.exports.expectedMessages[ message.type ] ){
-        console.log('heard a message:',message);
-      }
-    });
+  dictVectMapping: {
+    // this will be given to us by DictVectorizer, a python module that takes dictionaries and turns them into arrays. Obviously since dictionaries are not ordered, we need to keep track of which fields end up in which indices. 
   },
 
-  generatePythonOptions: function(fileNameFromRoot, otherArgs) {
-    // the first argument for all python shells is going to be a path to a file, relative to the root of ppLib
-    var fullPathToFile = global.rootDir + '/' + fileNameFromRoot;
-    var args = [];
-    args = args.concat(fullPathToFile, otherArgs);
-    return {
-      scriptPath: rfLocation,
-      args: args,
-      mode:'json'
-    };
+  fileNames: {
+    // this will be given to us by dataFormatting.py once it has created the files with the formatted data.
+    // ID
+    // X_train
+    // y_train
+    // X_test
+    // y_test
+    // X_train_normalized- used by neural networks. we will use the same ID and y_train files as the rest of the dataset. It is only the input features that have to be normalized, not the output features. 
+    // X_test_normalized- used by neural networks. we will use the same ID and y_train files as the rest of the dataset. It is only the input features that have to be normalized, not the output features. 
+    // trainingDataLength- technically not a file name, but fits much more logically here than reading in that file again in node.js
   },
 
-  startPythonShell: function(scriptName, callback, pythonOptions) {
-    var pyShell = PythonShell.run(scriptName, pythonOptions, function (err, results) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('successfully finished running',scriptName + '!');
-
-      }
+  formatData: function( callback ) {
+    // the callback function will be invoked with an object that holds the fileNames needed by module.exports.fileNames
+    df({
+      trainingData: argv.dataFile,
+      testingData: argv.kagglePredict,
+      trainingPrettyName: argv.outputFileName,
+      testingPrettyName: argv.testOutputFileName
+    }, function(fileNames) {
+      // df takes in a callback function that will be invoked with the fileNames object, holding the names and locations of the files it saved the data into
+      module.exports.fileNames = fileNames;
       callback();
     });
 
-    module.exports.attachLogListener(pyShell);
-    py.referencesToChildren.push(pyShell);
+  },
 
-    return pyShell;
+  kickOffTraining: function( callback, classifierName) {
+    // console.log('fileNames:',module.exports.fileNames);
+    // TODO: investigage if we have to refactor how we pass in file names?
+    var pythonOptions = utilsPyShell.generatePythonOptions(argv.dataFile, [JSON.stringify(argv), JSON.stringify(module.exports.fileNames), classifierName]);
+
+    var emitFinishedTrainingCallback = function() {
+      process.emit('algoFinishedTraining');
+      callback();
+    }
+
+
+    var pyShell = utilsPyShell.startPythonShell('training.py', emitFinishedTrainingCallback, pythonOptions);
+    // pyShell.on('message', function(message) {
+    //   if(message.type === 'trainingResults') {
+    //     process.emit('algoFinishedTraining');
+    //     global.trainedAlgos[classifierName] = message.text;
+    //   }
+    // });
+  },
+
+  makePredictions: function( callback, classifierName) {
+    console.log('kicking off the process of making predictions on the predicting data set for:', classifierName);
+
+    // TODO: 
+    var startPredictionsScript = function() {
+      var pythonOptions = utilsPyShell.generatePythonOptions(argv.kagglePredict, [module.exports.dictVectMapping, JSON.stringify(argv), JSON.stringify(module.exports.fileNames), classifierName]);
+
+      utilsPyShell.startPythonShell('makePredictions.py', callback, pythonOptions);
+    };
+
+    startPredictionsScript();
+
+
+    // TODO: we will already have the data formatted for us by data-formatter, so we can probably skip right to invoking startPredictionsScript. 
+
+    // reads our predict file, formats it, and then invokes startPredictionsScript as it's callback
+    // right now we are formatting the file multiple times, where we should only have to format that data once. 
+    // former todo: throw in a flag for whether we've already formatted the prediction data or not. obviously, if we have, use it, skip over invoking formatData again, and just invoke startPredictionsScript directly. 
+    // module.exports.formatData( startPredictionsScript, 'predict')
+
   }
 
 }
