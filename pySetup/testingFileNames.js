c82b8ac5dc63cfcf1f0be9daf47973c3f5cc13c1
ClimbsRocks <climbsbytes@gmail.com>
2015-10-28 22:00:51 -0700

    changes testing file names to match the new format for data-formatter

--- pySetup/testingFileNames.js
+++ pySetup/testingFileNames.js
@@ -1,11 +1,12 @@
-module.exports = { id_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_train_train.csv',
-  y_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/y_train_train.csv',
-  id_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_test_test.csv',
-  X_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_test_test.csv',
-  X_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_train_train.csv',
-  X_train_nn: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_train_nn_train.csv',
-  X_test_nn: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_test_nn_test.csv',
+module.exports = { 
+  id_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_train_giveCredittrain.csv',
+  y_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/y_train_giveCredittrain.csv',
+  id_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/id_test_giveCredittest.csv',
+  X_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_test_giveCredittest.csv',
+  X_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_train_giveCredittrain.csv',
+  X_train_nn: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_train_nn_giveCredittrain.csv',
+  X_test_nn: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/X_test_nn_giveCredittest.csv',
   brainJS_test: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/brainJS_test_test.csv',
   brainJS_train: '/Users/preston/ghLocal/machineLearningWork/ppComplete/pySetup/data-formatterResults/brainJS_train_train.csv',
-  trainingDataLength: 891 
+  trainingDataLength: 150000 
 };
