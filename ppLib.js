ebce35761670a33a159830d9251f18fa96a97ffe
ClimbsRocks <climbsbytes@gmail.com>
2015-11-08 13:02:25 -0800

    minor code cleanup

--- ppLib.js
+++ ppLib.js
@@ -1,4 +1,3 @@
-var path = require('path');
 global.argv = require('minimist')(process.argv.slice(1));
 var path = require('path');
 global.rootDir = path.dirname(__filename);
@@ -11,15 +10,15 @@ var ensembler = require('ensembler');
 
 console.log('thanks for inviting us along on your machine learning journey!\n');
 
-// TODO: refactor further so that we set all the default values in another file
 
 processArgs();
 
 if (argv.devEnsemble) {
-  ensembler.startListeners(numberOfClassifiers, argv.dataFilePretty, './predictions', argv.ppCompleteLocation );
+  // ensembler.startListeners(numberOfClassifiers, argv.dataFilePretty, './predictions', argv.ppCompleteLocation );
   ensembler.createEnsemble( argv.dataFilePretty, './predictions', argv.ppCompleteLocation );
 } else {
 
+  // console.log('argv in ppLib.js:', argv);
   controllerPython.startTraining(argv);
   
 }
