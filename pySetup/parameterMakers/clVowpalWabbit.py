53f84f78e8213abae8d557227561badd44057db1
ClimbsRocks <climbsbytes@gmail.com>
2015-11-08 09:15:21 -0800

    adds some good resources for when we build out vowpal wabbit

---
+++ pySetup/parameterMakers/clVowpalWabbit.py
@@ -0,0 +1,10 @@
+
+# some comments on good parameters:
+    # https://www.reddit.com/r/MachineLearning/comments/1mq8fb/why_i_love_scikitlearn/
+
+# a sklearn wrapper for vw:
+    # https://github.com/josephreisinger/vowpal_porpoise/blob/master/examples/example_sklearn.py
+
+# presentation with some good explanations of vw params:
+    # http://www.slideshare.net/odsc/owen-zhangopen-sourcetoolsanddscompetitions1
+
