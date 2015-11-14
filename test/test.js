e598751f2b3eea32eb29da70296ec5dfbefbbf85
ClimbsRocks <climbsbytes@gmail.com>
2015-11-13 22:23:09 -0800

    creates very rough skeleton of test suite

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var expect = require('chai').expect;
+var mocha = require('mocha');
+
+// this block will contain all the tests for the entire data-formatter package
+describe('data-formatter', function() {
+
+  // this timeout should be long enough to handle tests on a variety of machines. If you are getting a timeout error, consider bumping this up even more. 
+  this.timeout(600000);
+
+  setDefaultArgs();
+
+  // splitDataset();
+
+  // trainAlgos();
+
+  // makePredictions();
+
+  
+});
