8142b114c3014ea2363bfe63974340b66b51c9fe
ClimbsRocks <climbsbytes@gmail.com>
2015-11-15 15:36:25 -0800

    refactors before block into it's own module, along with setting default values

---
+++ test/regression/deleteRemnantsAndRunNewTest.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var execSync = require('child_process').execSync;
+var rimraf = require('rimraf');
+var fs = require('fs');
+
+// set default values
+// since node.js executes the file as node loads the file in, these lines of code will get run every time, regardless of whether we run the module.exports function or not
+global.rTest.testFileLocation = path.dirname(__filename);
+global.rTest.mjsLocation = path.join(rTest.testFileLocation, '..','..');
+global.rTest.dataLocation = path.join(rTest.mjsLocation,'node_modules','data-for-tests','rossman');
+
+rTest.dfTestResultsLocation = path.join(rTest.testFileLocation, 'dfTestResults');
+rTest.rTestPredictionsLocation = path.join(rTest.testFileLocation, 'rTestPredictions');
+rTest.bestClassifiersTestLocation = path.join(rTest.testFileLocation, 'bestClassifiersTest');
+rTest.validationIndicesLocation = path.join(rTest.dataLocation, 'dfValidationIndicesrossmantest.pkl');
+
+
+module.exports = function() {
+ try {
+    // remove any folders we might have created when running the test suite previously
+    // rimraf is `rm -rf` for node
+    rimraf.sync(rTest.dfTestResultsLocation);
+    rimraf.sync(rTest.rTestPredictionsLocation);
+    rimraf.sync(rTest.bestClassifiersTestLocation);
+    fs.unlinkSync(rTest.validationIndicesLocation);
+  } catch(err) {
+    // do nothing! There is nothing to delete
+  }
+
+  // to see detailed output while running the tests, use node-inspector.
+    // npm install -g node-inspector
+    // change "node" below to be "node-debug"
+  execSync('node ppLib.js ' 
+    + path.join(rTest.dataLocation,'tinyTrain.csv') 
+    + ' --kagglePredict ' + path.join(rTest.dataLocation,'test.csv') 
+    + ' --join ' + path.join(rTest.dataLocation, 'store.csv') 
+    + ' --dfOutputFolder ' + path.join(rTest.testFileLocation, 'dfTestResults')
+    + ' --predictionsFolder ' + path.join(rTest.testFileLocation, 'rTestPredictions')
+    + ' --ensemblerOutputFolder ' + rTest.testFileLocation
+    + ' --bestClassifiersFolder ' + path.join(rTest.testFileLocation, 'bestClassifiersTest')
+  );
+};
--- test/regression/test.js
+++ test/regression/test.js
@@ -1,67 +1,33 @@
+global.rTest = {};
 var expect = require('chai').expect;
 var mocha = require('mocha');
-var execSync = require('child_process').execSync;
 var path = require('path');
 var fs = require('fs');
-var rimraf = require('rimraf');
 
 var makePredictions = require('./makePredictions');
 var splitDataset = require('./splitDataset');
 var trainAlgorithms = require('./trainAlgorithms');
+var deleteRemnantsAndRunNewTest = require('./deleteRemnantsAndRunNewTest');
 
-var testFileLocation = path.dirname(__filename);
-var dataLocation = path.join(testFileLocation, '..','..','node_modules','data-for-tests','rossman');
-
-console.log('dataLocation',dataLocation);
 
 // this block will contain all the tests for the entire data-formatter package
 describe('regression problems', function() {
+  // this timeout should be long enough to handle tests on a variety of machines. If you are getting a timeout error, consider bumping this up even more. 
   this.timeout(600000);
 
-  before(function(done) {
+  rTest.startTime = Date.now();
 
-    try {
-      // remove any folders we might have created when running the test suite previously
-      // rimraf is `rm -rf` for node
-      rimraf.sync(path.join(testFileLocation, 'dfTestResults'));
-      rimraf.sync(path.join(testFileLocation, 'regressionTestPredictions'));
-      rimraf.sync(path.join(testFileLocation, 'bestClassifiersTest'));
-      fs.unlinkSync(path.join(dataLocation, 'dfValidationIndicesrossmantest.pkl'));
-      
-    } catch(err) {
-    }
+  // before(deleteRemnantsAndRunNewTest);
 
-    // to see detailed output while running the tests, use node-inspector.
-      // npm install -g node-inspector
-      // change "node" below to be "node-debug"
-    execSync('node ppLib.js ' 
-      + path.join(dataLocation,'tinyTrain.csv') 
-      + ' --kagglePredict ' + path.join(dataLocation,'test.csv') 
-      + ' --join ' + path.join(dataLocation, 'store.csv') 
-      + ' --dfOutputFolder ' + path.join(testFileLocation, 'dfTestResults')
-      + ' --predictionsFolder ' + path.join(testFileLocation, 'regressionTestPredictions')
-      + ' --ensemblerOutputFolder ' + testFileLocation
-      + ' --bestClassifiersFolder ' + path.join(testFileLocation, 'bestClassifiersTest')
-    );
 
-    done();
-
-  });
-
-  // this timeout should be long enough to handle tests on a variety of machines. If you are getting a timeout error, consider bumping this up even more. 
-
-  // TODO: it might make the most sense to just run machineJS all the way through, and then run tests on the final output results
-    // this would contrast with what we're doing in data-formatter, where the childProcess is emitting events at the end of each step.
   // TODO: run this separately for each type of problem we're solving (regression, category, then eventually multi-labe, etc.)
-  // global.testNamespace.mjsChildProcess = 
 
   // setDefaultArgs();
-
-  splitDataset();
 
   trainAlgorithms();
 
   makePredictions();
 
+  splitDataset();
 
 });
