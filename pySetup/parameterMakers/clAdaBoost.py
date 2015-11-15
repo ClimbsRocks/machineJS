210f75723fddb0076634bbe9dfa6154185ca9720
ClimbsRocks <climbsbytes@gmail.com>
2015-11-15 09:18:02 -0800

    switches back to GridSearchCV params

--- pySetup/parameterMakers/clAdaBoost.py
+++ pySetup/parameterMakers/clAdaBoost.py
@@ -3,17 +3,17 @@ import scipy
 def makeParams(X, y, globalArgs, dev, problemType):
 
     # GridSearchCV parameters:
-    # parameters_to_try = {
-    #     'n_estimators': [5,50,150],
-    #     'learning_rate': [.1, .3],
-    #     'algorithm':['SAMME','SAMME.R']
-    # }
+    parameters_to_try = {
+        'n_estimators': [5,50,150],
+        'learning_rate': [.1, .3],
+        'algorithm':['SAMME','SAMME.R']
+    }
 
     # RandomizedSearchCV parameters:
     # ideally, I think this would be a gamma distribution most likely. 
-    parameters_to_try = {
-        "n_estimators": scipy.stats.randint(25,150)
-    }
+    # parameters_to_try = {
+    #     "n_estimators": scipy.stats.randint(25,150)
+    # }
 
     if problemType != 'category':
         parameters_to_try.pop('algorithm', None)
