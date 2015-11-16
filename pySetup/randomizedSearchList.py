0b804ca1ba5dc89e15ec52709ba3f09558626aaf
ClimbsRocks <climbsbytes@gmail.com>
2015-11-15 22:14:04 -0800

    adds all existing classifiers to list as being unsupported

--- pySetup/randomizedSearchList.py
+++ pySetup/randomizedSearchList.py
@@ -3,5 +3,15 @@
 
 def rsList():
     return {
+        'clnnSknn': False,
+        'clnnNoLearn': False,
+        'clXGBoost': False,
+        'clKnn': False,
+        'clRfEntropy': False,
+        'clRfGini': False,
+        'clSVCFirst': False,
+        'clSVCShrinking': False,
+        'clnnSknn3Layer': False,
+        'clLogisticRegression': False,
         'clAdaBoost': True
     }
