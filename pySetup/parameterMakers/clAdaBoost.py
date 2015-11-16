d6358a7dae871fa4d1928235e5f99bcd363e46fd
ClimbsRocks <climbsbytes@gmail.com>
2015-11-15 22:08:11 -0800

    much more robust RandomizedSearchCV parameters

--- pySetup/parameterMakers/clAdaBoost.py
+++ pySetup/parameterMakers/clAdaBoost.py
@@ -1,22 +1,31 @@
 import scipy
+import numpy as np
 
 def makeParams(X, y, globalArgs, dev, problemType):
 
     # GridSearchCV parameters:
-    parameters_to_try = {
-        'n_estimators': [5,50,150],
-        'learning_rate': [.1, .3],
-        'algorithm':['SAMME','SAMME.R']
-    }
+    # parameters_to_try = {
+    #     'n_estimators': [5,50,150],
+    #     'learning_rate': [.1, .3],
+    #     'algorithm':['SAMME','SAMME.R']
+    # }
 
     # RandomizedSearchCV parameters:
     # ideally, I think this would be a gamma distribution most likely. 
-    # parameters_to_try = {
-    #     "n_estimators": scipy.stats.randint(25,150)
-    # }
+    
+
+    parameters_to_try = {
+        "n_estimators": scipy.stats.randint(25,500),
+        "learning_rate": scipy.stats.expon(.0001, 2),
+        "loss": ['linear','square','exponential'],
+        "algorithm": ['SAMME','SAMME.R']
+    }
 
     if problemType != 'category':
-        parameters_to_try.pop('algorithm', None)
+        try:
+            parameters_to_try.pop('algorithm', None)
+        except:
+            pass
 
     if dev:
         parameters_to_try.pop('learning_rate', None)
