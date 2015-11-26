91346c9ac9822ce929c35f722c6dda1d144b241e
ClimbsRocks <climbsbytes@gmail.com>
2015-11-25 22:32:23 -0800

    refactors to be usable from the command line and through require

--- ppLib.js
+++ ppLib.js
@@ -1,25 +1,49 @@
-global.argv = require('minimist')(process.argv.slice(1));
-var path = require('path');
-global.rootDir = path.dirname(__filename);
+#!/usr/bin/env node
 
-var controllerPython = require('./pySetup/controllerPython.js');
-var shutDown = require('./shutDown.js');
-var processArgs = require('./processArgs.js');
+(function() {
 
-var ensembler = require('ensembler');
+  var path = require('path');
+  global.rootDir = path.dirname(__filename);
+  global.argv = {};
 
-console.log('thanks for inviting us along on your machine learning journey!\n');
+  var controllerPython = require('./pySetup/controllerPython.js');
+  var shutDown = require('./shutDown.js');
+  var processArgs = require('./processArgs.js');
 
+  var ensembler = require('ensembler');
 
-processArgs();
+  console.log('thanks for inviting us along on your machine learning journey!\n');
 
-if (argv.devEnsemble) {
-  ensembler.createEnsemble( argv.ensemblerArgs );
-} else if( argv.makePredictions ) {
-  controllerPython.makeAllPredictions( argv.makePredictions );
-} else {
-  controllerPython.startTraining(argv);  
-}
+  module.exports = function(argsObj) {
+    if(argsObj !== undefined) {
+      for(var key in argsObj) {
+        global.argv[key] = argsObj[key];
+      }
+    }
 
-shutDown(controllerPython);
+    processArgs();
 
+    if (argv.devEnsemble) {
+      ensembler.createEnsemble( argv.ensemblerArgs );
+    } else if( argv.makePredictions ) {
+      controllerPython.makeAllPredictions( argv.makePredictions );
+    } else {
+      controllerPython.startTraining(argv);  
+    }
+
+    shutDown(controllerPython);
+  
+  };
+
+  // allow the module to be invoked from the command line
+  // since this is all wrapped in an IIFE, this if statement will execute and check if machineJS was invoked from another module, or without a parent (from the command line)
+  if( !module.parent ) {
+    var userArgs = require('minimist')(process.argv.slice(1));
+    for( var key in userArgs ) {
+      global.argv[key] = userArgs[key];
+    }
+
+    module.exports();
+  }
+
+})();
