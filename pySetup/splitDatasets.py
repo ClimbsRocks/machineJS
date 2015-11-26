0ad53e5a6b787b1a9d910016edcfacaf6e8b0bf3
ClimbsRocks <climbsbytes@gmail.com>
2015-11-25 17:48:24 -0800

    finally fixes validation split bug.

--- pySetup/splitDatasets.py
+++ pySetup/splitDatasets.py
@@ -12,6 +12,10 @@ from sendMessages import printParent
 from sendMessages import messageParent
 from sendMessages import obviousPrint
 
+
+# TODO TODO: make sure we are only creating training files if we have not already created teh validation indices
+    # TODO TODO: this means we probably just grab all teh file names, message them to the parent, and then return so we're not doing anything else.
+
 # TODO: Pass in these variables:
 args = json.loads(sys.argv[2])
 fileNames = json.loads(sys.argv[3])
@@ -39,20 +43,20 @@ X = load_sparse_csr(XFileName)
 
 numRows = X.shape[0]
 
-printParent('numRows: ' + str(numRows))
-
 includeOrNot = [random.random() for x in range(0,numRows)]
 
 validationIndexFolder = path.dirname(args['kagglePredict'])
 validationIndexFileName = 'dfValidationIndices' + args['testOutputFileName'] + '.pkl'
 validationIndicesFile = path.join( validationIndexFolder, validationIndexFileName )
 writeToFile = True
+createNewSplit = False
 try:
     with open(validationIndicesFile, 'rb') as openFile:
         validationIndices = pickle.load(openFile)
         maxVal = max( validationIndices )
         # check to make sure that the validation length is less than the length of our X dataset
-        if len(validationIndices) > numRows * ( validationPercent - .02):
+        if len(validationIndices) > numRows * ( validationPercent + .02):
+            printParent('validationIndices too long')
             # if it isn't, create a new validationIndices for this dataset, but do not write it to file
             # this lets us keep our larger validationIndices split (for the full training data set), while still having something to work with for this smaller dataset we're currently testing on.
             writeToFile = False
@@ -60,13 +64,17 @@ try:
 
         # check to make sure that the validation length is within a few percentage points of our validationPercent number (in other words, if X is 10,000 rows long, and the largest number in validationIndices is only 1,200, then we know validationIndices was built on a smaller test dataset earlier.)
         elif len(validationIndices) < numRows * validationPercent * .98:
+            printParent('validationIndices too short')
             # If it is not, create a new validationIndices and write that to file
             raise IndexError("this dataset is longer than the one we built the validation split on previously")
             
         # In both cases, fall into the except state below
         # but create a variable that lays out whether to write that new validationIndices to file or not in the try block, and then use that in the except block below
 
+
+
 except:
+    createNewSplit = True
     validationIndices = []
     trainingIndices = []
     for idx, randomNum in enumerate(includeOrNot):
@@ -106,17 +114,18 @@ def splitDataset(data, name, fileCategory):
 
     # if this "sparse" matrix only has a single value for each row, we have to treat it as a column matrix, and slice it accordingly
     # this is the case for our idColumn, and frequently our y values as well.
-    if data.shape[0] == 1:
-        # search = data[:,searchIndices]
-        # longTrainingData = data[:,trainingDataIndices]
-        validation = data[:,validationIndices]
-        trainingData = data[:,trainingIndices]
+    if createNewSplit:
+        if data.shape[0] == 1:
+            # search = data[:,searchIndices]
+            # longTrainingData = data[:,trainingDataIndices]
+            validation = data[:,validationIndices]
+            trainingData = data[:,trainingIndices]
 
-    else:
-        # search = data[searchIndices,:]
-        # longTrainingData = data[trainingDataIndices,:]
-        validation = data[validationIndices,:]
-        trainingData = data[trainingIndices,:]
+        else:
+            # search = data[searchIndices,:]
+            # longTrainingData = data[trainingDataIndices,:]
+            validation = data[validationIndices,:]
+            trainingData = data[trainingIndices,:]
 
     name = ntpath.basename(name)
     name = name[0:-4]
@@ -126,10 +135,11 @@ def splitDataset(data, name, fileCategory):
     validationFile = path.join(outputDirectory, name + 'validationData.npz')
     trainingDataFile = path.join(outputDirectory, name + 'trainingData.npz')
 
-    # save_sparse_csr(searchFile, search)
-    # save_sparse_csr(longTrainingFile, longTrainingData)
-    save_sparse_csr(validationFile, validation)
-    save_sparse_csr(trainingDataFile, trainingData)
+    if createNewSplit:
+        # save_sparse_csr(searchFile, search)
+        # save_sparse_csr(longTrainingFile, longTrainingData)
+        save_sparse_csr(validationFile, validation)
+        save_sparse_csr(trainingDataFile, trainingData)
 
     fileNameDict = {
         fileCategory + 'trainingData': trainingDataFile,
