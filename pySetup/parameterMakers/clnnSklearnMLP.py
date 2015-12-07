7de57f13c66558c100964f062822a38241d6975b
ClimbsRocks <climbsbytes@gmail.com>
2015-12-06 21:55:37 -0800

    adds in hidden_layer options up to 10 hidden layers and 10x numFeatures per hidden layer

--- pySetup/parameterMakers/clnnSklearnMLP.py
+++ pySetup/parameterMakers/clnnSklearnMLP.py
@@ -7,6 +7,8 @@ def makeParams(X, y, globalArgs, dev, problemType):
     # guidance on params:
         # http://www.slideshare.net/odsc/owen-zhangopen-sourcetoolsanddscompetitions1
 
+    
+
     try:
         # if dense
         numFeatures = len(X[0])
@@ -23,6 +25,20 @@ def makeParams(X, y, globalArgs, dev, problemType):
         # adam is optimized stochastic gradient descent which performs well for large datasets
         algorithmType = 'adam'
 
+    # we want to test up to 10 layers, with anywhere from .5 * numFeatures to 10*numFeatures per layer
+    hiddenLayers = []
+
+    # for each hidden layer, we will have these numbers * numFeatures number of nodes
+    nodeMultipliers = [.5,1,2,3,5,10]
+
+    # we will create options for each number of hidden layers listed below
+    hiddenLayerMultipliers = [1,2,3,5,10]
+
+    for hlNum in hiddenLayerMultipliers:
+        for nodeNum in nodeMultipliers:
+            hiddenLayers.append( [ nodeNum * numFeatures for x in range(hlNum) ])            
+
+
     # parameters we are not searching currently:
         # tol
         # shuffle
@@ -30,6 +46,7 @@ def makeParams(X, y, globalArgs, dev, problemType):
         # hidden_layer_sizes
 
     parameters_to_try = {
+        'hidden_layer_sizes': hiddenLayers,
         'activation': ['logistic','tanh','relu'],
         'algorithm': [algorithmType],
         'alpha': scipy.stats.expon(.00001,.001),
